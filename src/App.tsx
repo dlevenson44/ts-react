070a43d930da69e089d984953cfe423a30a03714
dlevenson44 <daniellevenson4@gmail.com>
2020-03-09 15:40:28 -0400

    Setup App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import './App.css';
 
-const App: React.SFC = () => (
-  <div className="App">
-    Hello World
-  </div>
-);
+const App:FunctionComponent<{ initial?: number }> = ({ initial = 0}) => {
+  const [counter, setCounter] = useState(initial);
+  return(
+    <div className="App">
+      <p>TypeScript Counter with Hooks</p>
+      {counter}
+    </div>
+  )
+}
 
 export default App;
