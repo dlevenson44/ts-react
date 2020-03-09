a8123a37436c12232e997f164a117cf4e4e2d7ec
dlevenson44 <daniellevenson4@gmail.com>
2020-03-09 15:14:58 -0400

    Move hookless counter app, reset SRC folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,10 @@
 import React from 'react';
 import './App.css';
-import Title from './components/Title';
-import Button from './components/Button';
 
-// JSX vs TSX in action
-// const App = ({ message }) => <div className="App">{message}</div>
-// const App: React.SFC<AppProps> = ({ message }) => (
-//   <div className="App">
-//     <Title title={message} />  
-//   </div>
-// );
-
-type AppProps = { message: string }
-type AppState = { counter: number, increaseTitle: string, decreaseTitle: string }
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      counter: 0,
-      increaseTitle: "Increase Count",
-      decreaseTitle: "Decrease Count"
-    }
-  }
-  increment = () => {
-    this.setState({
-      counter: (this.state.counter + 1)
-    })
-  }
-  decrement = () => {
-    this.setState({
-      counter: (this.state.counter - 1)
-    })
-  }
-  render() {
-    return(
-      <div className="App">
-        <Title title={this.props.message} />
-        <Title title={this.state.counter} />
-        <Button title={this.state.increaseTitle} clicker={this.increment} />
-        <Button title={this.state.decreaseTitle} clicker={this.decrement} />
-      </div>
-    );
-  }
-}
+const App: React.SFC = () => (
+  <div className="App">
+    Hello World
+  </div>
+);
 
 export default App;
