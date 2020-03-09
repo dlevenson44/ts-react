0dcd0d1f725c26768943bd436657893b071e37ae
dlevenson44 <daniellevenson4@gmail.com>
2020-03-09 15:45:21 -0400

    Finish counter app with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useState } from 'react';
 import './App.css';
+import Button from './components/Button';
 
 const App:FunctionComponent<{ initial?: number }> = ({ initial = 0}) => {
   const [counter, setCounter] = useState(initial);
@@ -7,6 +8,8 @@ const App:FunctionComponent<{ initial?: number }> = ({ initial = 0}) => {
     <div className="App">
       <p>TypeScript Counter with Hooks</p>
       {counter}
+      <Button title="Increase Count" clicker={() => setCounter(counter + 1)} />
+      <Button title="Decrease Count" clicker={() => setCounter(counter - 1)} />
     </div>
   )
 }
